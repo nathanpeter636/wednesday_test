48f3921da219190cefd82501b606aa7ada4df267
nathanpeter636 <nathanpeter636@gmail.com>
2020-03-12 11:08:28 +1300

    fist commit

--- src/components/Getdata.jsx
+++ src/components/Getdata.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import Axios from "axios";
 
 
+import { animations } from 'react-animation'
+
+
+
+
+
 
 import "../css/getdata.css";
 
@@ -12,6 +18,8 @@ import { MdDelete } from "react-icons/md";
 import { FaUserEdit } from "react-icons/fa";
 import { IoIosAddCircle } from "react-icons/io";
 
+const mystyle = {color: "red"}
+
 class GetData extends React.Component {
   constructor(props) {
     super(props);
@@ -19,6 +27,7 @@ class GetData extends React.Component {
     // console.log("hello from getData component");
 
     this.state = { users: [] };
+    // this.state = { FadeOut: false }
   }
 
   componentDidMount() {
@@ -30,18 +39,24 @@ class GetData extends React.Component {
 
     Axios.get("http://localhost:4000/api/users").then(res => {
       console.table(res.data);
-      this.setState({ users: res.data });
+      this.setState({ users: res.data, deleted: false });
     });
   };
 
+
+
   render() {
+
+    // const DeleteFade = this.state.FadeOut
+
+
     return (
       <div className="user_wrapper">
         <p>USERS</p>
 
         <Link to="/addusers">
           <IoIosAddCircle
-            style={{ color: "rgb(102, 235, 112)", fontSize: "3.8em" }}
+            style={{ color: "lime", fontSize: "3.8em" }}
           />
         </Link>
 
@@ -63,16 +78,31 @@ class GetData extends React.Component {
 
 export class ShowData extends React.Component {
   //deleting user
+  constructor(props) {
+    super(props);
+
+    // console.log("hello from getData component");
+
+
+    // this.state = { FadeOut: false }
+    this.state = ({ deleted: false });
+  }
 
   deleteData = event => {
     console.log("deleteData");
 
+    // this.state = {  deleteButtonClicked: false};
+
     Axios.delete(`http://localhost:4000/api/users/${this.props.id}`).then(
       res => {
         console.table(res.data);
+
         if (res.data.result === true) {
-          this.props.getData()
+
+          this.setState({deleted: true})
+          
         }
+        
       }
     )
   };
@@ -86,14 +116,20 @@ export class ShowData extends React.Component {
   // };
 
   render() {
+    
+    
+     
+   
     return (
-      <div className="user_container">
+      <div   className={this.state.deleted === true ? "user_container ": "user_container"} style={{animation: animations.popIn}}>
         <img src="https://placekitten.com/100/100" alt=""/>
         <h1>
-          {this.props.name} {this.props.lastname}{" "}
+          {this.props.name} {this.props.lastname}
+
+
         </h1>
 
-        {/* <button onClick={this.deleteData} > Delete </button> */}
+       
         <MdDelete
           onClick={this.deleteData}
           style={{ color: "rgb(232, 61, 23)", fontSize: "1.8em" }}
@@ -106,7 +142,12 @@ export class ShowData extends React.Component {
             
           />
         </Link>
+        
+
+        
       </div>
+
+      
     );
   }
 }
